2d2eacfe9fb047e211435d4c53d9a912c111260b
Oleksandr Borysenko <kontynium@upstars.com>
2025-05-13 15:26:07 +0200

    add new img

--- src/components/1_HeroSection.vue
+++ src/components/1_HeroSection.vue
@@ -204,20 +204,7 @@ onUnmounted(() => {
   }
 }
 
-.noice {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 10;
-  background-image: url('/img/noise.svg');
-  background-size: 6.25rem;
-  background-position: center;
-  background-repeat: repeat;
-  mix-blend-mode: color-burn;
-  pointer-events: none;
-}
+
 
 
 
