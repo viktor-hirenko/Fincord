dc50c2d42120308b24fead03f12fa8f2b01fc76f
Oleksandr Borysenko <kontynium@upstars.com>
2025-05-14 11:09:59 +0200

    add ready desctop

--- src/components/ComingSoon.vue
+++ src/components/ComingSoon.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="coming-soon-container">
-    <h1>Coming Soon</h1>
-    <p>We're working on this page. Please check back later.</p>
+    <h1>coming soon</h1>
+    <p class="stay">stay</p>
+    <p class="tuned">tuned</p>
+    <div class="coming_soon_img_wrapper">
+      <img src="/img/lines.svg" alt="coming soon" class="lines">
+    </div>
   </div>
 </template>
 
@@ -11,26 +15,86 @@
 
 <style lang="scss" scoped>
 .coming-soon-container {
+  position: relative;
+  top: 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height: calc(100vh - 6.25rem - 6.25rem); /* Вычитаем высоту хедера и футера */
+  min-height: calc(100vh); /* Вычитаем высоту хедера и футера */
   padding: 2rem;
   background-color: #000;
+  @supports (min-height: 100dvh) {
+    min-height: calc(100dvh);
+  }
   
   h1 {
-    font-size: 3rem;
-    margin-bottom: 1rem;
-    color: #C9B36E;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: #F2F2F2;
     text-align: center;
+    font-size: 3rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    @media (max-width: 768px) {
+      white-space: nowrap;
+      font-size: 1.5rem;
+    }
   }
   
   p {
-    font-size: 1.2rem;
-    color: #fff;
-    text-align: center;
-    max-width: 600px;
+    color: #F2F2F2;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    @media (max-width: 768px) {
+      font-size: 0.75rem;
+    }
   }
 }
+
+.stay {
+  position: absolute;
+  bottom: 2rem;
+  left: 6rem;
+  @media (max-width: 768px) {
+    left: 2rem;
+  }
+}
+
+.tuned {
+  position: absolute;
+  bottom: 2rem;
+  right: 6rem;
+  @media (max-width: 768px) {
+    right: 2rem;
+  }
+}
+
+
+
+.coming_soon_img_wrapper {
+  position: absolute;
+  height: 1000px;
+  width: auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(var(--scale-value));
+}
+
+.lines {
+  position: absolute;
+  top: 0;
+  left: -928px;
+  width: 1200px;
+  height: 100%;
+  transform: perspective(800px) rotateY(-54deg);
+}
+
+
+
 </style> 
--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="main_wrapper">
     <ComingSoon />
+    <Footer />
   </div>
   <div class="noise"></div>
 </template>
 
 <script setup lang="ts">
 import ComingSoon from '../components/ComingSoon.vue'
+import Footer from '../components/7_Footer.vue'
 </script>
 
 <style scoped></style> 
